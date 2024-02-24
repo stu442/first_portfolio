25c1a7b004166651eb0630972b21a209bdc944b4
GJG <stu442@naver.com>
2024-02-24 16:55:21 +0900

    feat : github pages 배포

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,7 +2,7 @@ interface ProjectData {
     index: number
     title: string
     description: string
-    homepage: string
+    homepage: string | null
     github: string
 }
 
@@ -24,7 +24,7 @@ export default function Project() {
         {
             title: "저기어때",
             description: "여기어때를 클론코딩한 프로젝트",
-            homepage: "존재하지 않음",
+            homepage: null,
             github: "https://github.com/jugiaddae-front/master"
         }
     ];
@@ -35,7 +35,7 @@ export default function Project() {
             <h2 className="text-4xl my-2">프로젝트</h2>
             <div className="">
                 {projects.map((data, index) => (
-                    <ProjectCard key={index} index={index} title={data.title} description={data.description} homepage={data.homepage} github={data.github} />
+                    <ProjectCard key={index} index={index} title={data.title} description={data.description} homepage={data?.homepage} github={data.github} />
                 ))}
             </div>
         </section>
@@ -54,8 +54,8 @@ function ProjectCard({index, title, description, homepage, github} : ProjectData
                 <h3 className="text-3xl my-2">{title}</h3>
                 <h4 className="text-lg font-medium text-white/70">{description}</h4>
             </div>
-            <div className="flex justify-between my-4">
-                <a href={homepage} target="_blank"><button className="bg-green px-12 py-4 rounded-xl">홈페이지</button></a>
+            <div className={`flex ${homepage ? "justify-between" : "justify-center"} my-8`}>
+                {homepage && <a href={homepage} target="_blank"><button className="bg-green px-12 py-4 rounded-xl">홈페이지</button></a>}
                 <a href={github} target="_blank">
                     <button className="flex gap-2 justify-center bg-gray-600 px-12 py-4 rounded-xl">
                         <img className="w-6" src="/src/icons/github.svg" />
